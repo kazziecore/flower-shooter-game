9338265e57068078de850f1268ed5c55d3789c0a
kazziecore <C24308593@myTUDublin.ie>
2025-04-05 16:24:25 +0100

    updated enemy timer
    felt like enemies spawned too fast

--- flower-shooter-game/scripts/enemy.gd
+++ flower-shooter-game/scripts/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player = $/root/Game/player
 
-const speed = 100
+const speed = 80
 
 func _physics_process(_delta: float) -> void:
 	velocity = (player.global_position - global_position).normalized() * speed
