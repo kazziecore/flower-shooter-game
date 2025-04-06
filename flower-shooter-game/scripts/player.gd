009ee5e4935e92497ff673ba0abf9776d3e99760
kazziecore <C24308593@myTUDublin.ie>
2025-04-06 14:23:39 +0100

    added sound effects

--- flower-shooter-game/scripts/player.gd
+++ flower-shooter-game/scripts/player.gd
@@ -4,6 +4,7 @@ var bullet_scene = preload("res://scenes/bullet.tscn")
 const speed = 200
 @onready var is_reloading = false
 @onready var shooty_part = $shootypart
+@onready var bulletsound = $bulletsound
 
 func _physics_process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -17,11 +18,12 @@ func _physics_process(_delta: float) -> void:
 		bullet.global_position = shooty_part.global_position
 		bullet.direction = (get_global_mouse_position() - global_position).normalized()
 		$/root/Game.add_child(bullet)
+		bulletsound.play()
 
 	move_and_slide()
 	
 	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(1)
+		var collision = get_slide_collision(i)
 		
 		if collision.get_collider().is_in_group("enemies") and not is_reloading:
 			is_reloading = true
